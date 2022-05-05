23ecef8722fc8852bda3e153c6e94dbf969ae34e
jracina316 <jamesr316@yahoo.com>
2022-05-04 17:40:17 -0700

    2022-05-04 CodeWars

---
+++ 8-squaringAnArgument.js
@@ -0,0 +1,7 @@
+//Now you have to write a function that takes an argument and returns the square of it.
+
+const square = num1 => num1**2
+
+//Most popular answer matches
+
+const square = (n) => n * n;
